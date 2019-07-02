eac18de7b9842103e8f6191efc4b55e3b57f376d
Koudjo Aholou <ah.koudjo@gmail.com>
2019-07-02 20:55:40 +0200

    console + let var const

--- console/App.js
+++ console/App.js
@@ -1,6 +1,8 @@
 console.time("Temps necessaire recup H2");
 const allH2 = document.querySelectorAll(('h2'))
 
+//CONSOLE
+
 // Afficher le texte des h2
 for(i=0; i<allH2.length; i++){ document.write(allH2[i].textContent +" ")}
 console.log(allH2,document.querySelector(('h1')));
@@ -16,4 +18,22 @@ console.log(2)
 console.log(3)
 console.groupEnd("Groupe 1 console log");
 
-console.log("autre console log")
+console.log("autre console log");
+
+//LET CONST VAR
+
+let tylerDurder; //ok
+var marlaSinger; // ok
+//const robertPaulson; //!error not possible with const
+
+const robertPaulson = {alive :true};
+const pair = {isNameIs: "Paulson"};
+console.log("Robert",robertPaulson)
+robertPaulson.alive = false; //*ok
+console.log("Dead Robert",robertPaulson)
+//robertPaulson.isNameIs = "Robert Paulson"; // * working with const
+//console.log(robertPaulson, "2 proprieties");
+
+Object.assign(robertPaulson, {isNameIs: "Robert Paulson"}); // *working with const Object assign
+//robertPaulson = {...robertPaulson, ...pair }  // !not working with const spread operator
+console.log(robertPaulson, "2 proprieties bis");
